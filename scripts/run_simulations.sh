753668b8ccfeb306bfdc2dae40d078d017b37313
Pascal Merz <pascal.merz@colorado.edu>
2022-06-09 23:38:37 +0200

    Initial input files, run simulation script, and test (#1)
    This commit includes the input files for the different simulation
    stages and phases, as well as the topology and configuration files for
    the protein and ligand complexes. All input files were thereby broken
    down as much as possible, to detail the similarities between the
    simulations and simplify maintenance.
    
    This commit also includes a run_simulation script which helps to
    combine the input files for the different simulation stages and phases
    and runs simulations. This script is tested by a dedicated test
    script, which is also called on every push and pull request to main
    via GitHub actions. This commit also contains the set up for this
    action, including a conda environment and a workflow.
    
    Finally, this updates the README file with an initial documentation of
    the repository and how to use it.

---
+++ scripts/run_simulations.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+function usage()
+{
+  echo "USAGE: run_water_nes [-h] -d dir -t dir [-c crd] -x gmx [-o opts] -s N -p {min,eqNVT,eqNPT,prod}"
+}
+
+function help()
+{
+  echo "Run Water NES simulations."
+  echo
+  usage
+  echo "Options:"
+  echo -e "-h\tPrint this help and exit"
+  echo -e "-d dir\tThe base working directory for the system"
+  echo -e "-t top\tThe topology directory including topology files"
+  echo -e "-c crd\tThe input coordinates, for minimization only"
+  echo -e "-x gmx\tThe GROMACS executable to use"
+  echo -e "-o opts\tThe run options, e.g. \"-nt 16 -ntomp 4\", optional"
+  echo -e "-s N\tThe stage to run, one of:"
+  echo -e "\t1: Stage with fully interacting, non-restrained water"
+  echo -e "\t2: Stage with fully interacting, restrained water"
+  echo -e "\t3: Stage with non-interacting, restrained water"
+  echo -e "-p {min,eqNVT,eqNPT,prod}"
+  echo -e "\tThe simulation phase to run, one or more of: min, eqNVT, eqNPT, prod"
+  echo -e "\tSeparate different phases by spaces, e.g. -p \"min eqNVT eqNPT\"".
+  echo
+}
+
+function fail()
+{
+  echo -e "ERROR: $1"
+  exit 1
+}
+
+# Get the options
+while getopts "d:t:c:x:o:s:p:h" option; do
+  case $option in
+    d) BASEDIR="${OPTARG}";;
+    t) TOPDIR="${OPTARG}";;
+    c) CRD="${OPTARG}";;
+    x) GMX="${OPTARG}";;
+    o) RUN_PARAMS="${OPTARG}";;
+    s) STAGE="${OPTARG}";;
+    p) PHASES="${OPTARG}";;
+    h) help; exit 0;;
+    *) usage; exit 1;;
+  esac
+done
+
+IMPLEMENTED_STAGES="1 2 3"
+IMPLEMENTED_PHASES="min eqNVT eqNPT prod"
+
+# Check presence of positional arguments
+if [ -z "$BASEDIR" ] || [ -z "$TOPDIR" ] || [ -z "$GMX" ] || [ -z "$STAGE" ] || [ -z "$PHASES" ]; then
+  usage
+  exit 1
+fi
+
+# Check option values
+mkdir -p $BASEDIR
+[ -d "$BASEDIR" ] || fail "Directory $BASEDIR does not exist."
+[ -d "$TOPDIR" ] || fail "Topology directory $TOPDIR does not exist."
+which $GMX &> /dev/null || fail "Executable $GMX not found."
+
+VALID_STAGE=0
+for stage in $IMPLEMENTED_STAGES; do
+  [ "$STAGE" == "$stage" ] && VALID_STAGE=1
+done
+[ $VALID_STAGE -eq 1 ] || fail "Stage $STAGE is not a valid choice."
+
+for phase in $PHASES; do
+  if [[ "$IMPLEMENTED_PHASES" != *"$phase"* ]]; then
+    fail "Unknown phase $phase"
+  fi
+  if [ "$phase" == "min" ]; then
+    if [ -z "$CRD" ] || [ ! -e "$CRD" ]; then
+      fail "Minimization phase requires a valid coordinate input file."
+    fi
+  fi
+done
+
+# Get full paths
+BASEDIR=$(readlink -f $BASEDIR)
+TOPDIR=$(readlink -f $TOPDIR)
+[ ! -z "$CRD" ] && CRD=$(readlink -f $CRD)
+
+# Check presence of needed topology files for all stages and phases
+for file in system waterConstraintLambdaIndependent waterConstraintLambdaDependent; do
+  [ -e $TOPDIR/$file.top ] || fail "File $file.top not found in directory $TOPDIR"
+done
+
+# Check presence of mdp input files for all stages and phases
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+INPUTDIR=$SCRIPT_PATH/../input
+[ -d $INPUTDIR ] || fail "Input directory $INPUTDIR not found."
+for stage in $IMPLEMENTED_STAGES; do
+  [ -e $INPUTDIR/stage$stage.mdp ] || fail "Input file $INPUTDIR/stage$stage.mdp is missing."
+done
+for phase in $IMPLEMENTED_PHASES; do
+  [ -e $INPUTDIR/$phase.mdp ] || fail "Input file $INPUTDIR/$phase.mdp is missing."
+done
+
+
+# Function that runs grompp and mdrun for given directory, mdp file, and gro file
+function run_simulation()
+{
+  local WORKDIR=$1
+  local MDP=$2
+  local GRO=$3
+  local TOP=$4
+
+  STARTDIR=$PWD
+
+  cd $WORKDIR || fail "Could not access directory $WORKDIR."
+
+  $GMX grompp -c "$GRO" -p "$TOP" -f "$MDP" || \
+    fail "grompp command failed:\n\t$GMX grompp -c \"$GRO\" -p \"$TOP\" -f \"$MDP\"\n\t(wd: $PWD)"
+  $GMX mdrun $RUN_PARAMS || \
+    fail "mdrun command failed:\n\t$GMX mdrun $RUN_PARAMS\n\t(wd: $PWD)"
+
+  cd $STARTDIR || fail "Could not access directory $STARTDIR."
+}
+
+# Launch simulations for all requested phases
+for phase in $PHASES; do  
+  echo "Running phase $phase ..."
+
+  # Create work directory if it doesn't exist
+  WORKDIR=$BASEDIR/$phase
+  mkdir -p $WORKDIR
+  
+  # Pick input coordinates for phase
+  case $phase in
+    "min") GRO=$CRD;;
+    "eqNVT") GRO=$BASEDIR/min/confout.gro;;
+    "eqNPT") GRO=$BASEDIR/eqNVT/confout.gro;;
+    "prod") GRO=$BASEDIR/eqNPT/confout.gro;;
+    *) echo "Unknown phase";;
+  esac
+  [ -e $GRO ] || fail "Phase $phase cannot be run because coordinate file $GRO is missing."
+  
+  # Create input file for phase & stage
+  MDP=$WORKDIR/input.mdp
+  cp $INPUTDIR/$phase.mdp $MDP || fail "Error creating input file"
+  cat $INPUTDIR/stage$STAGE.mdp >> $MDP || fail "Error creating input file"
+
+  # Create topology file for phase & stage
+  TOP=$WORKDIR/topology.top
+  cp $TOPDIR/system.top $TOP || fail "Error creating topology file"
+  if [ $STAGE -eq 3 ]; then
+    CONSTRAINT_FILE=waterConstraintLambdaIndependent.top
+  else
+    CONSTRAINT_FILE=waterConstraintLambdaDependent.top
+  fi
+  echo >> $TOP
+  cat $TOPDIR/$CONSTRAINT_FILE >> $TOP || fail "Error creating topology file"
+  
+  run_simulation $WORKDIR $MDP $GRO $TOP
+
+done
