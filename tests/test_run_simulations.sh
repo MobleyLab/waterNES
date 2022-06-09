bc4da401a2321fc7ba7342edb08bc7920665b6a6
Pascal Merz <pascal.merz@me.com>
2022-06-09 22:48:34 +0200

    Initial input files, run simulation script, and test
    This commit includes the input files for the different simulation
    stages and phases, as well as the topology and configuration files for
    the protein and ligand complexes. All input files were thereby broken
    down as much as possible, to detail the similarities between the
    simulations and simplify maintenance.
    
    This commit also includes a run_simulation script which helps to
    combine the input files for the different simulation stages and phases
    and runs simulations. This script is tested by a dedicated test
    script, which is also called on every push and pull request to main
    via GitHub actions. This commit also contains the set up for this
    action, including a conda environment and a workflow.
    
    Finally, this updates the README file with an initial documentation of
    the repository and how to use it.

---
+++ tests/test_run_simulations.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function fail()
+{
+  echo -e "ERROR: $1"
+  exit 1
+}
+
+# We expect this to run from the root of the repository
+[ -e tests/$(basename $0) ] || fail "Expected to be in the root repository."
+
+RUN_SCRIPT=scripts/run_simulations.sh
+# Check that we can find the script we want to test
+[ -e $RUN_SCRIPT ] || fail "Can't find script $RUN_SCRIPT."
+
+# Check that GROMACS is available as `gmx`
+GMX=gmx
+which $GMX &> /dev/null || fail "Executable $GMX not found."
+
+TEST_SYSTEM=1HPX
+WORKDIR=systems/$TEST_SYSTEM
+TOPDIR=$WORKDIR
+CRD=$WORKDIR/system.gro
+# Check that typical usage runs without errors, using very small simulations to be fast
+bash $RUN_SCRIPT -d $WORKDIR/sys2 -t $TOPDIR -c $CRD -x $GMX -o "-nt 1 -nsteps 2" -s 2 -p min || fail "Error in simulation."
+bash $RUN_SCRIPT -d $WORKDIR/sys2 -t $TOPDIR -c $CRD -x $GMX -o "-nt 1 -nsteps 2" -s 2 -p eqNVT || fail "Error in simulation."
+bash $RUN_SCRIPT -d $WORKDIR/sys2 -t $TOPDIR -c $CRD -x $GMX -o "-nt 1 -nsteps 2" -s 2 -p eqNPT || fail "Error in simulation."
+bash $RUN_SCRIPT -d $WORKDIR/sys2 -t $TOPDIR -c $CRD -x $GMX -o "-nt 1 -nsteps 2" -s 2 -p prod || fail "Error in simulation."
+# Run the other systems in one go
+bash $RUN_SCRIPT -d $WORKDIR/sys1 -t $TOPDIR -c $CRD -x $GMX -o "-nt 1 -nsteps 2" -s 1 -p "min eqNVT eqNPT prod" || fail "Error in simulation."
+bash $RUN_SCRIPT -d $WORKDIR/sys3 -t $TOPDIR -c $CRD -x $GMX -o "-nt 1 -nsteps 2" -s 3 -p "min eqNVT eqNPT prod" || fail "Error in simulation."
+
+# If we reached here, all commands passed without error!
+echo "SUCCESS: All simulations ran without error."
+exit 0
