566588710fee95c79fa7a5c17c11c685434be876
Pascal Merz <pascal.merz@me.com>
2022-09-12 16:43:23 +0200

    NES restart

---
+++ hpc3/restart_nes_simulation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=DMOBLEY_LAB
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1-00:00:00
+
+# The source command can be replaced by loading a module (if the GROMACS version you
+# want to use is available) or by a pointer to any other GROAMCS installation.
+# The next line is telling shellcheck not to worry about the sourced file.
+# shellcheck source=/dev/null
+source ~/bin/gmx2022.2+/bin/GMXRC
+
+gmx mdrun -ntomp 1 -cpi state.cpt
